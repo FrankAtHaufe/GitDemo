af71190074cafa7fb50721f4d6208f9dd57b60e8
Frank Wittmann <frank.wittmann@haufe-lexware.com>
2017-11-29 08:51:13 +0100

    added Assembly LogeAppModel.dll

--- GitDemo/LeanFtTest.cs
+++ GitDemo/LeanFtTest.cs
@@ -5,7 +5,7 @@ using HP.LFT.Verifications;
 using HP.LFT.SDK.WinForms;
 using System.Drawing;
 using System.Windows.Forms;
-
+using LogeAppModel;
 
 namespace GitDemo
 {
@@ -28,6 +28,9 @@ namespace GitDemo
         public void Test()
         {
             //MessageBox.Show("Yeppee, Unit Test is running fine...");
+            var x = new LogeAppModel.ClassStandardDialog();
+
+
         }
 
         [TearDown]
